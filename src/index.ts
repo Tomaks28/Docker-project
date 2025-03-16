30e4b132de14258347231d706df3081045d9eace
Thomas <56254260+Tomaks28@users.noreply.github.com>
2025-03-16 17:02:14 +0100

    feat: wip

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,11 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express with docker and TS 🔥");
 });
 
+app.get("/error", (req: Request, res: Response) => {
+  throw new Error("Error route");
+  exit(1);
+});
+
 app.listen(4000, () => {
   console.log(`Server is running on port ${4000}`);
 });
